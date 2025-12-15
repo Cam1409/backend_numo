06d312b28c122ecea685025e5eab9b6b4599e5b7
Cam1409 <eocanaba14@ucvvvirtual.edu.pe>
2025-12-15 16:55:21 -0500

    ALFINLLEGOOOELDIA

--- wapp/settings.py
+++ wapp/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://backend-numo.onrender.com', 'https://app-finanzas-opal.vercel.app'] 
+CSRF_TRUSTED_ORIGINS = ['https://backend-numo.onrender.com', 'https://app-finanzas-i3uc.vercel.app'] 
 
 ROOT_URLCONF = 'wapp.urls'
 
@@ -151,7 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://app-finanzas-opal.vercel.app"
+    "https://app-finanzas-i3uc.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
